6fcfe4a82ec48a66276ab490068c098d4bd7934d
Faruk Yucel <farukyucel5@gmail.com>
2022-12-21 00:39:44 +0300

    SWAG

--- src/test/java/Exercises/Swag.java
+++ src/test/java/Exercises/Swag.java
@@ -1,17 +1,16 @@
 package Exercises;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Swag {
 
-    WebDriver driver;
-    @Before
-    public void first(){
+    static WebDriver driver;
+    @BeforeClass
+    public static void first(){
         WebDriverManager.chromedriver().setup();
         driver=new ChromeDriver();
         driver.manage().window().maximize();
@@ -27,8 +26,17 @@ public class Swag {
         driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
         driver.findElement(By.xpath("//input[@id='login-button']")).click();
         driver.findElement(By.xpath("//button[@id='react-burger-menu-btn']")).click();
-        driver.findElement(By.xpath("//a[@id='reset_sidebar_link']")).click();
+    }
 
+    @Test
+    public void test1(){
+
+        driver.findElement(By.xpath("//a[@id='reset_sidebar_link']")).click();
+    }
+
+    @AfterClass
+    public static void close(){
+        driver.quit();
     }
 
 
