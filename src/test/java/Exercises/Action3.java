2e7af58aee407bb88045be2964c8108ef1383679
Faruk Yucel <farukyucel5@gmail.com>
2022-12-23 15:26:36 +0300

    updating

--- src/test/java/Exercises/Action3.java
+++ src/test/java/Exercises/Action3.java
@@ -1,53 +1,31 @@
 package Exercises;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.locators.RelativeLocator;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
+import java.time.Duration;
 
-public class Actions_2 {
+public class Action3 {
 
     public static void main(String[] args) throws InterruptedException {
         WebDriverManager.chromedriver().setup();
         WebDriver driver=new ChromeDriver();
         driver.manage().window().maximize();
 
-        driver.get("https://www.etsy.com/");
+        driver.get("https://demoqa.com/tool-tips");
 
-        Actions actions=new Actions(driver);
+        Actions actions1=new Actions(driver);
+        WebElement element=driver.findElement(By.xpath("//button[@id='toolTipButton']"));
 
-        List<WebElement> menubar=driver.findElements(By.xpath("//ul[@role='menubar']//li"));
-
-        for (WebElement each:menubar) {
-            actions.moveToElement(each).perform();
-            Thread.sleep(1000);
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        actions1.moveToElement(element).build().perform();
+      driver.close();
     }
 }
